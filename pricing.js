5a724cf543ae906549b7cbd4d2749a85796eb965
Harsh1518 <harshanand064@gmail.com>
2022-09-09 19:22:59 +0530

    Pricing part added

--- pricing.js
+++
@@ -1,9 +0,0 @@
-const faqHeaders = document.querySelectorAll(".faqs-container .faq-header");
-
-
-faqHeaders.forEach((header, i)=>{
-    header.addEventListener("click", ()=>{
-        header.nextElementSibling.style.backgroundColor = "black";
-        header.nextElementSibling.classList.toggle("active");
-    });
-});
